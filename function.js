f9b5c12a3492cefe47987c5786cc8365d5ffcde8
JudyGichungu <35097105+JudyGichungu@users.noreply.github.com>
2018-05-24 17:24:40 +0300

    Add files via upload
    function.js

---
+++ function.js
@@ -0,0 +1,23 @@
+function concat(word1, word2, word3){
+	const space = ' ';
+  return a + space + b + space + c;
+
+
+	}
+  
+	let a='Jane', b='Doe', c=19
+  
+	let student=concat(a,b,c);
+  
+  console.log(student)
+
+
+
+
+
+  function equals(var1,var2){
+
+return var1==var2;
+
+}
+	
